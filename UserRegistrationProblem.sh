e468cce607bca1672c8582fb27be0fb0039f3312
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-29 19:14:02 +0530

    ADDED:Function To Check Valid First Name

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -2,3 +2,23 @@
 
 #DISPLAYING WELCOME MESSAGE
 echo "Welcome To User Registration Problem"
+
+#VARIABLE
+FirstNamePattern=0;
+
+#PATTERN
+firstNamePattern="^[A-Z][A-Za-z]{2,}$"
+
+#FUNCTION TO CHECK VALID FIRST NAME
+function firstNameValidation() {
+	read -p "Enter First Name: " firstName
+	if [[ $firstName =~ $firstNamePattern ]]
+	then
+		echo "Valid Input"
+	else
+		echo "Invalid Input"
+	fi
+}
+
+#CALLING FUNCTION TO VALIDATE FIRST NAME
+firstNameValidation
