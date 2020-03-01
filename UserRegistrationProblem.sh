4f0b13b3376130f381d1a6c4ffa736e5524d4d54
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-01 14:52:41 +0530

    ADDED:Function To Validate Mobile Number

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -10,11 +10,13 @@ emailId=0;
 firstNamePattern=0;
 lastNamePattern=0;
 emailPattern=0;
+mobileNumber=0;
 
 #PATTERN
 firstNamePattern="^[A-Z][A-Za-z]{2,}$"
 lastNamePattern="^[A-Z][A-Za-z]{2,}$"
 emailPattern="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
+mobileNumberPattern="^[0-9]{2}[ ][0-9]{10}$"
 
 #FUNCTION TO CHECK VALID FIRST NAME
 function firstNameValidation() {
@@ -57,3 +59,18 @@ function emailValidation() {
 
 #CALLING FUNCTION TO VALIDATE EMAIL ID
 emailValidation
+
+#FUNCTION TO VALIDATE USER MOBILE NUMBER
+function mobileNumberValidation() {
+	read -p "Enter Country Code And Mobile Number " mobileNumber
+	if [[ $mobileNumber =~ $mobileNumberPattern ]]
+	then
+		echo "Valid Mobile Number"
+	else
+		echo "Invalid Mobile Number"
+	fi
+}
+
+#CALLING FUNCTION TO VALID MOBILE NUMBER
+mobileNumberValidation
+
